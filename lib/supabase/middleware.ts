a231163bed343ca9238dd130f34992775f9ae04e
Manoj Kumar Jena <jenamanojkumar1997@gmail.com>
2026-06-15 02:46:22 +0530

    Remove debug logs

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -3,16 +3,6 @@ import { NextResponse, type NextRequest } from "next/server";
 import type { Database } from "@/types";
 
 
-console.log(
-  "SUPABASE_URL:",
-  !!process.env.NEXT_PUBLIC_SUPABASE_URL
-);
-
-console.log(
-  "SUPABASE_KEY:",
-  !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
